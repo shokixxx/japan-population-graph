d3e1322cfe569eb20a1c93b6eeaa947b8cd4a6cb
shokixxx <uver_discord_0703@yahoo.co.jp>
2024-01-11 08:55:11 +0900

    headerの追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import utilStyles from '../styles/utils.module.css'
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className={utilStyles.headingL}>都道府県別の総人口推移グラフ</h1>
+    </header>
+  )
+}
+
+export default Header
