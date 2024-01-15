f08e23033ea1a2633434f896274014644e137f32
shokixxx <uver_discord_0703@yahoo.co.jp>
2024-01-15 12:08:22 +0900

    読み込み時に都道府県取得APIを呼び出し、CheckBoxに渡す

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
+import { useEffect, useState } from 'react'
+
 import CheckBox from './components/CheckBox'
 import Header from './components/Header'
 
+type Prefectures = {
+  prefCode: number
+  prefName: string
+}
+
 const App = () => {
-  const prefectures = ['北海道', '青森県']
+  const [prefectures, setPrefectures] = useState<Prefectures[]>([])
+
+  useEffect(() => {
+    const getPrefectures = async () => {
+      await fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+        headers: {
+          'X-API-KEY': `${process.env.REACT_APP_API_X_API_KEY}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((prefecture) => setPrefectures(prefecture.result))
+    }
+    getPrefectures()
+  }, [])
   return (
     <>
       <div className="App">
         <Header />
         <main>
           <h2>都道府県</h2>
-          {prefectures.map((prefecture, index) => (
-            <CheckBox key={index} label={prefecture} />
-          ))}
+          {prefectures &&
+            prefectures.map((prefecture) => (
+              <CheckBox key={prefecture.prefCode} label={prefecture.prefName} />
+            ))}
         </main>
       </div>
     </>
