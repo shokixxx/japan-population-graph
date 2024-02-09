6993a79e0ea3cdd7294d892825fbef67dedddf7e
shokixxx <uver_discord_0703@yahoo.co.jp>
2024-02-09 12:08:49 +0900

    人口構成を取得するAPIの修正

--- src/pages/api/prefecture.ts
+++ src/pages/api/prefecture.ts
@@ -10,6 +10,12 @@ export type Prefecture = {
   prefName: string
 }
 
+/**
+ * 都道府県に関する都道府県データを返します
+ * @returns PrefectureData[] 都道府県データの配列
+ * @property prefCode 都道府県コード
+ * @property prefName 都道府県名
+ */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Prefecture[]>
@@ -35,6 +41,6 @@ export default async function handler(
     return
   }
 
-  const data: PrefectureResponse = await resusRes.json()
-  res.status(200).json(data.result)
+  const prefecturesData: PrefectureResponse = await resusRes.json()
+  res.status(200).json(prefecturesData.result)
 }
