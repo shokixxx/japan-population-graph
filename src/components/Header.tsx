aa29bbe271e0851b5dc77094d0ba733718033c70
nishinohi <tomoya.nishi@azone-japan.co.jp>
2024-02-10 01:32:23 +0900

    Headとheaderの意味を正しく理解できていない気がする

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,13 @@
-import Head from 'next/head'
-
 import layout from '../styles/layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
 const Header = () => {
   return (
-    <>
-      <Head>
-        <title>都道府県別人口グラフ</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <header>
       <h1 className={`${utilStyles.headingL} ${layout.header}`}>
         都道府県別の総人口推移グラフ
       </h1>
-    </>
+    </header>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import Head from 'next/head'
 import Layout from '../components/Layout'
 
 const Home = () => {
-  return <Layout />
+  return (
+    <>
+      <Head>
+        <title>都道府県別人口グラフ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout />
+    </>
+  )
 }
 
 export default Home
